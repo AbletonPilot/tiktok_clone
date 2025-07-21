4f2c6a82fe0929953c7af03aa41318801d7d9c2f
Logan <sonjm0428@gmail.com>
2025-07-21 15:33:18 +1000

    Sign Up Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/screens/sign_up/sign_up_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -11,9 +12,10 @@ class TikTokApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'TicTok Clone',
-      theme: ThemeData(primaryColor: Colors.blue),
-      home: Container(),
+      theme: ThemeData(primaryColor: const Color(0xFFE9435A)),
+      home: const SignUpScreen(),
     );
   }
 }
